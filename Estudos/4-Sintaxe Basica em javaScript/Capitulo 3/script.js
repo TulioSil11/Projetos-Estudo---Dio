54427d950059ea97d555818235cc67e318ea4bee
Tulio Silva <0dizinx@gmail.com>
2021-12-25 08:32:03 -0300

    Atualizaçao/Projeto de criação de uma calculadora

---
+++ Estudos/4-Sintaxe Basica em javaScript/Capitulo 3/script.js
@@ -0,0 +1,13 @@
+//Tipos Primitivos
+
+//Boolean
+var  vOuF = false;
+console.log(typeof(vOuF));
+
+//Number
+var numeroQualquer = 1;
+console.log(typeof(numeroQualquer));
+
+//String
+var nome = 'Tulio';
+console.log(typeof(nome));
