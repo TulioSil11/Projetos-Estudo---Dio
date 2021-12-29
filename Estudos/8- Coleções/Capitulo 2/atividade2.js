3739592ba55f77744418235efd9d97595c2431f7
Tulio Silva <0dizinx@gmail.com>
2021-12-29 06:36:19 -0300

    inclusao do Curso de coleçoes

---
+++ Estudos/8- Coleções/Capitulo 2/atividade2.js
@@ -0,0 +1,8 @@
+const meuArray = [30, 30, 40, 5, 223, 2049, 3034, 5];
+function valoresUnicos(arr) {
+    const mySet = new Set(arr);
+
+    return [...arr];
+}
+
+console.log(valoresUnicos(meuArray));
